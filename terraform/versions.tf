e18d82b14a1f18241f129a4e5788bfbb5266a12f
Vandana <vandana.b6871@gmail.com>
2026-09-13 23:59:36 +0530

    Add Terraform archive provider

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -13,10 +13,15 @@
 terraform {
   required_version = ">= 1.11.0" # 1.11+ required for S3 native locking (use_lockfile)
 
-  required_providers {
+   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.0"
     }
   }
 
